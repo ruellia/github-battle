013f204546e5ca8be8472a1e9baf5e0c66b22ef4
Ruellia Zhang <syncyes@gmail.com>
2017-02-26 21:56:37 -0800

    basic routing

--- app/index.js
+++ app/index.js
@@ -1,48 +1,5 @@
-var USER_DATA = {
-    name: 'Ruellia Zhang',
-    username: 'ruellia',
-    image: 'http://texassuperstar.com/plants/ruellia/ruellia3.jpg'
-}
-
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return(
-      <div>
-        <a href={'https://www.github.com/' +  this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    );
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return <div>{this.props.name}</div>
-  }
-});
-
-var Avatar = React.createClass({
-  render: function() {
-    return(
-      <div>
-        <ProfilePic imageUrl={this.props.user.image}/>
-        <ProfileName name={this.props.user.name}/>
-        <ProfileLink username={this.props.user.username}/>
-      </div>
-    );
-  }
-});
-
-
-ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
