2b2701cf2964e7eca3fdfaee0bb24e8e366a150b
Ruellia Zhang <syncyes@gmail.com>
2017-02-26 21:29:02 -0800

    basic nested components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,48 @@
+var USER_DATA = {
+    name: 'Ruellia Zhang',
+    username: 'ruellia',
+    image: 'http://texassuperstar.com/plants/ruellia/ruellia3.jpg'
+}
+
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-    render: function() {
-        return(
-            <div> Hello World! </div>
-        )
-    }
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
 });
 
-ReactDOM.render(
-    <HelloWorld />,
-    document.getElementById('app')
-);
+var ProfileLink = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <a href={'https://www.github.com/' +  this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
