360ae9b70a5f629012c19dc7fb36cba84bb32df3
Ruellia Zhang <syncyes@gmail.com>
2017-03-08 22:56:32 -0800

    add transition animations and fancier loading screen

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,6 +5,7 @@ var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
 var Link = require('react-router').Link;
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function StartOver() {
     return (
@@ -18,9 +19,7 @@ function StartOver() {
 
 function Results(props) {
     if (props.isLoading === true) {
-        return(
-            <p>LOADING</p>
-        )
+        return <Loading text="One moment" speed={100}/>
     }
     if (props.scores[0] === props.scores[1]) {
         return (
