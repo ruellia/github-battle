8a4b6b7d52e25ccb631a78c14e3239f79489c967
Ruellia Zhang <syncyes@gmail.com>
2017-03-08 22:28:43 -0800

    add results container and refactor

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -5,15 +5,12 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
-
-function puke(object) {
-    return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var MainContainer = require('./MainContainer');
 
 function ConfirmBattle(props) {
     return props.isLoading === true
         ? <p>LOADING</p>
-        : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        : <MainContainer>
             <h1>Confirm Players</h1>
             <div className='col-sm-8 col-sm-offset-2'>
                 <UserDetailsWrapper header='Player 1'>
@@ -36,7 +33,7 @@ function ConfirmBattle(props) {
                     </Link>
                 </div>
             </div>
-        </div>
+        </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
